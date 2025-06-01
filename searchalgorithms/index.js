898ae6b4d95375d086da42b6f6e4622f8e01097b
Michael obot📚⚙️♥️ <112352785+Michaelobot1@users.noreply.github.com>
2025-06-01 11:41:35 +0100

    implementation using javascript

---
+++ searchalgorithms/index.js
@@ -0,0 +1,23 @@
+//define the function search with parameter arr N X
+function search (arr, N, X){
+    for(i= 0 ; i < N ; i++){
+      if (arr[i] == X){
+        return i
+      } 
+    }
+    return -1
+}
+//initliaze the variables required,the arr, the length of the array N,the target value
+let arr = [1,1,1,2,4,5,6,7,8,6,7,6,5,5,7,7,3];
+let N = arr.length;
+let X = 3;
+
+//let results be equal to the function search call
+results = search(arr, N, X)
+//write a conditional statement based on the output of th search function
+if (results == -1){
+  console.log('your target value does not exist');
+}
+else{
+  console.log("your target value's index is", results);
+}
